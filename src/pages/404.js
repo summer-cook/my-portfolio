28b7a74efa574ab8aa13aa36f3738857f3d961fa
Summer Cook <summer@scientist.com>
2023-06-05 14:01:35 -0500

    fix error with head tags

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,21 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { Container } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
+import Head from './head'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
-const Head = () => {
-  return (
-    <>
-      <meta charSet="utf-8" />
-      <title>Page not found</title>
-      <html lang="en" />
-      <meta name="description" content="Page not found" />
-    </>
-  );
-};
-
-export const FourOhFour = () => {
+const FourOhFour = () => {
   return (
     <>
       <Head />
---
+++ src/pages/head.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/main.scss';
+
+const Head = () => {
+  return (
+    <>
+      <meta charSet="utf-8" />
+      <title>Page not found</title>
+      <html lang="en" />
+      <meta name="description" content="Page not found" />
+    </>
+  )
+}
+
+export default Head
