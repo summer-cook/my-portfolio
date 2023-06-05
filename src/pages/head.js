94e79ca6009ce460e3fc1a932b99e38a4de6c279
Summer Cook <summer@scientist.com>
2023-06-05 17:20:37 -0500

    fix the images

--- src/pages/head.js
+++ src/pages/head.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { Container } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
 
@@ -13,7 +10,7 @@ const Head = () => {
       <html lang="en" />
       <meta name="description" content="Page not found" />
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
